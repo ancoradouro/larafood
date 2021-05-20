bd671aebae0eae6f4d3ebd5dd51247e272ba1e17
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-05-20 02:21:14 -0300

    Teste

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('admin/plans', 'App\\Http\\Controllers\\Admin\\PlanController@index')->name('plans.index');
 
+
 Route::get('/', function () {
     return view('welcome');
 });
