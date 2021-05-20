35f294e81a072e4bece2321845904862369bf773
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-05-20 00:31:15 -0300

    03 - Listar os Planos do LaraFood

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -7,13 +7,29 @@
 @stop
 
 @section('content')
-    <p>Listagem dos planos.</p>
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
+    <div class="card">
+        <div class="card-header">
+            #filtros
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th style="width:50px;">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>{{ $plan->name }}</td>
+                            <td>{{ $plan->price }}</td>
+                            <td style="width:50px;"><a href="#" class="btn btn-warning">Ações</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop
