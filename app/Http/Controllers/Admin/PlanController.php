3054969ae7b195bda58728da9a96db97aeee78f2
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-05-21 00:23:10 -0300

    09 - Breadcrumb no LaraFood

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -18,7 +18,7 @@ class PlanController extends Controller
 
     public function index()
     {
-        $plans = $this->repository->latest()->paginate(5);
+        $plans = $this->repository->latest()->paginate(1);
 
         return view('admin.pages.plans.index', [
             'plans' => $plans,
@@ -40,4 +40,41 @@ class PlanController extends Controller
         return redirect()->route('plans.index');
     }
 
+    public function show($url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+
+        if (!$plan)
+            return redirect()->back();
+
+        return view('admin.pages.plans.show', [
+            'plan' => $plan
+        ]);
+    }
+
+    public function destroy($url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+
+        if (!$plan)
+            return redirect()->back();
+
+        $plan->delete();
+
+        return redirect()->route('plans.index');
+    }
+
+    public function search(Request $request)
+    {
+        //dd($request->all());
+        //$filters = $request->all();
+        $filters = $request->except('_token');
+
+        $plans = $this->repository->search($request->filter);
+
+        return view('admin.pages.plans.index', [
+            'plans' => $plans,
+            'filters' => $filters,
+        ]);
+    }
 }
--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -4,12 +4,21 @@
 
 @section('content_header')
     <h1>Planos <a href="{{ route('plans.create') }}" class="btn btn-dark">ADD</a> </h1>
+
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('plans.index') }}" class="active">Planos</a></li>
+    </ol>
 @stop
 
 @section('content')
     <div class="card">
         <div class="card-header">
-            #filtros
+        <form action="{{ route('plans.search') }}" method="post" class="form form-inline">
+                @csrf
+                <input type="text" name="filter" placeholder="Nome" class="form-control" value="{{ $filters['filter'] ?? '' }}">
+                <button type="submit" class="btn btn-dark">Filtrar</button>
+            </form>
         </div>
         <div class="card-body">
             <table class="table table-condensed">
@@ -24,15 +33,19 @@
                     @foreach ($plans as $plan)
                         <tr>
                             <td>{{ $plan->name }}</td>
-                            <td>{{ $plan->price }}</td>
-                            <td style="width:50px;"><a href="#" class="btn btn-warning">Ações</a></td>
+                            <td>R$ {{ number_format($plan->price, 2, ',', '.') }}</td>
+                            <td style="width:50px;"><a href="{{ route('plans.show', $plan->url) }}" class="btn btn-warning">Ações</a></td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
         <div class="card-footer">
-            {!! $plans->links() !!}
+            @if (isset($filters))
+                {!! $plans->appends($filters)->links() !!}
+            @else
+                {!! $plans->links() !!}
+            @endif
         </div>
     </div>
 @stop
