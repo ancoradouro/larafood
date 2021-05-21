fb9b911304daeca3a064f06396cfcff64346fc2e
bitts <marcelo.valvassori@gmail.com>
2021-05-21 00:36:55 -0300

    Ajustando paginação de forma inteligante

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 
 class Plan extends Model
 {
     //use HasFactory;
     protected $fillable = ['name', 'url', 'price', 'description'];
+    protected $num_pagination = 3;
+
+    public function __construct()
+    {
+        Paginator::useBootstrap();
+    }
+
+    public function search($filter = null)
+    {
+        return $this->where('name', 'LIKE', "%{$filter}%")
+                    ->orWhere('description', 'LIKE', "%{$filter}%")
+                    ->paginate($this->num_pagination);
+    }
+
 }
