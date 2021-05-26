ca6238bdf803ade26c3201ce525df48a1e9dbcd2
bitts <marcelo.valvassori@gmail.com>
2021-05-26 00:48:27 -0300

    Ajustando a inserção de valores

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdatePlan;
 use App\Models\Plan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PlanController extends Controller
 {
     private $repository;
-    protected $num_pagination = 4;
+    protected $num_pagination = 10;
 
     public function __construct(Plan $plan)
     {
@@ -31,16 +33,16 @@ class PlanController extends Controller
         return view('admin.pages.plans.create');
     }
 
-    public function store(Request $request)
+    public function store(StoreUpdatePlan $request)
     {
-        //dd($request->all());
-        $data = $request->all();
-        $data['url'] = Str::kebab($request->name);
-        //dd($data);
-        $this->repository->create($data);
+        $plan = new Plan();
+        $plan->name = $request->name;
+        $plan->description = $request->description;
+        $plan->url = Str::kebab($request->name);
+        $plan->price = $request->price;
+        $plan->save();
 
-        //$this->repository->create($request->all());
-
+        //return redirect('/index')->with('message', 'Plano criado com sucesso');
         return redirect()->route('plans.index');
     }
 
@@ -48,8 +50,7 @@ class PlanController extends Controller
     {
         $plan = $this->repository->where('url', $url)->first();
 
-        if (!$plan)
-            return redirect()->back();
+        if (!$plan)return redirect()->back();
 
         return view('admin.pages.plans.show', [
             'plan' => $plan
@@ -94,12 +95,11 @@ class PlanController extends Controller
         ]);
     }
 
-    public function update(Request $request, $url)
+    public function update(StoreUpdatePlan $request, $url)
     {
         $plan = $this->repository->where('url', $url)->first();
 
-        if (!$plan)
-            return redirect()->back();
+        if (!$plan)return redirect()->back();
 
         $plan->update($request->all());
 
